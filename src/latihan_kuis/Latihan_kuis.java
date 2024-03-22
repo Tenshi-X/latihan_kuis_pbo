608e39c01014ffc634ee0e9df2fcb8e91172f1fa
Tenshi-X <sevafarel17@gmail.com>
2024-03-22 22:36:08 +0700

    Add Project

---
+++ src/latihan_kuis/Latihan_kuis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan_kuis;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan_kuis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new mainMenu().setVisible(true);
+    }
+    
+}
